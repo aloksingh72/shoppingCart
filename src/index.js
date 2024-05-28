09ff384b2dc59f04a732ba573217d6ae168b79d2
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2024-05-28 18:38:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import {BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //creating the routes
+  // browser route
   <BrowserRouter>
    <Provider store={store}>
     <App />
